161ad11f62213824e5622cd2a30bd09c55ad9cf8
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2018-04-23 18:34:34 +0200

    Added Blazor demo

---
+++ blazor/BlazorDemo/Pages/ConferenceComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorDemo.Services;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace BlazorDemo.Pages
+{
+	public class ConferenceComponent : BlazorComponent
+	{
+		[Inject]
+		protected ConferenceService _conferenceService { get; set; }
+
+		protected IEnumerable<ConferenceModel> Conferences { get; set; }
+
+		protected override async Task OnInitAsync()
+		{
+			Conferences = await _conferenceService.LoadConferences();
+		}
+	}
+}
